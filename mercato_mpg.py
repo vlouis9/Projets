e0e46e1c055a2398831abca12693a1c5428a7b22
vlouis9 <louisvermeulen91@gmail.com>
2025-05-28 19:05:36 +0200

    Update mercato_mpg.py

--- mercato_mpg.py
+++ mercato_mpg.py
@@ -87,8 +87,8 @@ PREDEFINED_PROFILES = {
         "min_recent_games_played_filter": 2,
         "kpi_weights": {
             'GK': {'recent_avg': 0.15, 'season_avg': 0.4, 'calc_regularity': 0.25, 'recent_goals': 0.0, 'season_goals': 0.0},
-            'DEF': {'recent_avg': 0.1, 'season_avg': 0.4, 'calc_regularity': 0.25, 'recent_goals': 0.0, 'season_goals': 0.0},
-            'MID': {'recent_avg': 0.1, 'season_avg': 0.3, 'calc_regularity': 0.2, 'recent_goals': 0.05, 'season_goals': 0.15},
+            'DEF': {'recent_avg': 0.0, 'season_avg': 0.75, 'calc_regularity': 0.15, 'recent_goals': 0.0, 'season_goals': 0.10},
+            'MID': {'recent_avg': 0.0, 'season_avg': 0.6, 'calc_regularity': 0.1, 'recent_goals': 0.0, 'season_goals': 0.3},
             'FWD': {'recent_avg': 0.0, 'season_avg': 0.5, 'calc_regularity': 0.1, 'recent_goals': 0.0, 'season_goals': 0.4}
         },
         "mrb_params_per_pos": {
