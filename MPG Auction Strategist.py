63d20b99d8a33f79ff97fe95a47fe0b4f87d454b
vlouis9 <louisvermeulen91@gmail.com>
2025-06-20 09:48:25 +0200

    Update MPG Auction Strategist.py

--- MPG Auction Strategist.py
+++ MPG Auction Strategist.py
@@ -14,12 +14,13 @@ st.header("Choose Your Strategist Tool:")
 # --- Replace with your actual URLs ---
 URL_HISTORICAL_APP = "https://mercatompg.streamlit.app"
 URL_NEW_SEASON_APP = "https://newmercatompg.streamlit.app"
+URL_SANDBOX_APP = "https://newmpgseason.streamlit.app"
 
-col1, col2 = st.columns(2)
+col1, col2, col3 = st.columns(3)
 
 with col1:
-    st.subheader("📊 Historical Data App")
-    st.markdown("For leagues with past season performance data.")
+    st.subheader("📊 Ongoing Season App")
+    st.markdown("For leagues with season data.")
     if URL_HISTORICAL_APP != "YOUR_URL_FOR_THE_HISTORICAL_DATA_APP_HERE":
         st.link_button("Launch Historical App", URL_HISTORICAL_APP)
     else:
@@ -31,7 +32,15 @@ with col2:
     if URL_NEW_SEASON_APP != "YOUR_URL_FOR_THE_NEW_SEASON_APP_HERE":
         st.link_button("Launch New Season App", URL_NEW_SEASON_APP)
     else:
-        st.warning("New Season App URL not configured.")
+        st.warning("🏝️ New Season App URL not configured.")
+
+with col3:
+    st.subheader("🎯 SandBox")
+    st.markdown("To play with data and players")
+    if URL_SANDBOX_APP != "YOUR_URL_FOR_THE_SANDBOX_APP_HERE":
+        st.link_button("Launch Sandbox App", URL_SANDBOX_APP)
+    else:
+        st.warning("Sandbox App URL not configured.")
 
 st.sidebar.title("About")
 st.sidebar.info(
