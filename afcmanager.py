0bb64c1ee8673fedd279270e47c49e0b599549a2
vlouis9 <louisvermeulen91@gmail.com>
2025-07-10 20:34:54 +0200

    Update afcmanager.py

--- afcmanager.py
+++ afcmanager.py
@@ -1234,6 +1234,7 @@ with tab2:
         journees = sorted(st.session_state.championnat_scores.keys())
         if "selected_journee" not in st.session_state:
             st.session_state.selected_journee = journees[0]
+        selected = st.session_state.selected_journee
             
             idx = journees.index(st.session_state.selected_journee)
             col1, col2, col3 = st.columns([1, 4, 1])
